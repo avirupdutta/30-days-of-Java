0d5e070a80cde92c99ae601292c686a520ff43d9
avirupdutta <finalguider99@gmail.com>
2019-08-20 21:21:06 +0530

    learnt about interfaces

---
+++ InterfaceDemo.java
@@ -0,0 +1,40 @@
+public class InterfaceDemo {
+    public static void main(String args[]){
+        
+        Kit myKit = new Kit();
+        Pen p = new Pen();
+        Pencil pc = new Pencil();
+        myKit.doSomething(pc);
+
+
+    }
+}
+class Tools{
+    public void write(int a){
+        System.out.println("I am a tool");
+    }
+}
+
+interface Writer{
+    void write();
+}
+
+class Pen extends Tools implements Writer{
+    public void write(){
+        System.out.println("I am Pen");
+    }
+}
+
+class Pencil extends Tools implements Writer{
+    public void write(){
+        System.out.println("I am Pencil");
+    }
+}
+
+class Kit{
+    void doSomething(Writer p){
+        p.write();
+    }
+}
+
+
