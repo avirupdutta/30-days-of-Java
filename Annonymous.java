12ba55431d8fe452efcb531ce45622ceae6bcbb6
avirupdutta <finalguider99@gmail.com>
2019-08-21 20:17:21 +0530

    learnt about anonymous classes

---
+++ Annonymous.java
@@ -0,0 +1,27 @@
+public class Annonymous {
+    public static void main(String[] args) {
+        // Dog spike = new Dog(){
+        //     void bark(){
+        //         System.out.println("The baby dog is barking");
+        //     }
+        // };
+        // spike.bark();
+
+        Animal newAnimal = new Dog(){
+            public void bark(){
+                System.out.println("The small dog is barking");
+            }
+        };
+        newAnimal.bark(); 
+    }
+}
+
+interface Animal{
+    void bark();
+}
+class Dog implements Animal{
+    public void bark(){
+        System.out.println("The Dog is Barking!");
+    }
+}
+
