8777ba35ce30ef3deb11f0433f97815556fa4661
avirupdutta <finalguider99@gmail.com>
2019-08-23 21:26:00 +0530

    learnt about inner class and weird behaviour of static keyword

---
+++ InnerClassDemo.java
@@ -0,0 +1,29 @@
+public class InnerClassDemo {
+
+    public static void main(String args[]){
+        Apple.iPhone newIPhone = new Apple.iPhone("iPhone X");
+        newIPhone.showModelName();
+    }
+}
+
+public class Apple {
+
+    static String brandName = "Apple Inc.";
+
+    static class iPhone{
+        static int totalManufactures = 0;
+        private String modelName;
+
+        iPhone(String name){
+            modelName = name;
+            totalManufactures++;
+        }
+        public void showModelName(){
+            System.out.println(brandName+" has manufactured the iPhone with Model name: "+modelName);
+        }
+    }
+
+}
+
+
+
