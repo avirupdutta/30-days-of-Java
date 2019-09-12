11e11ba7ea78cc2d4e0badafbd7a39c314c3fa73
avirupdutta <finalguider99@gmail.com>
2019-09-12 20:40:22 +0530

    learnt about multithreading using lambda expressions.

--- src/MultithreadingDemo.java
+++ src/MultithreadingDemo.java
@@ -1,29 +1,29 @@
-class Hi extends Thread{
-    public void run (){
-        for (int i = 0; i < 15; i++) {
-            System.out.println("Hi");
-            try { Thread.sleep(1000); } catch (Exception e) { e.printStackTrace(); }
-        }
-    }
-}
-
-class Hello extends Thread{
-    public void run (){
-        for (int i = 0; i < 15; i++) {
-            System.out.println("Hello");
-            try { Thread.sleep(1000); } catch (Exception e) { e.printStackTrace(); }
-        }
-    }
-}
-
-
-public class MultithreadingDemo {
-    public static void main ( String[] args ) {
-        Hi obj1 = new Hi();
-        Hello obj2 = new Hello();
-
-        obj1.start();
-        try { Thread.sleep(10); } catch (Exception e) { e.printStackTrace(); }
-        obj2.start();
-    }
-}
+//class Hi extends Thread{
+//    public void run (){
+//        for (int i = 0; i < 15; i++) {
+//            System.out.println("Hi");
+//            try { Thread.sleep(1000); } catch (Exception e) { e.printStackTrace(); }
+//        }
+//    }
+//}
+//
+//class Hello extends Thread{
+//    public void run (){
+//        for (int i = 0; i < 15; i++) {
+//            System.out.println("Hello");
+//            try { Thread.sleep(1000); } catch (Exception e) { e.printStackTrace(); }
+//        }
+//    }
+//}
+//
+//
+//public class MultithreadingDemo {
+//    public static void main ( String[] args ) {
+//        Hi obj1 = new Hi();
+//        Hello obj2 = new Hello();
+//
+//        obj1.start();
+//        try { Thread.sleep(10); } catch (Exception e) { e.printStackTrace(); }
+//        obj2.start();
+//    }
+//}
