a55aa288d6f730ca858f3373c0fc93010a856b05
avirupdutta <finalguider99@gmail.com>
2019-09-21 12:03:34 +0530

    learnt about setting and getting the name and priority of threads

--- src/ThreadedInterface.java
+++ src/ThreadedInterface.java
@@ -7,18 +7,27 @@ class ThreadedInterface {
 	public static void main(String[] args) throws Exception{
 
 		// The Thread() constructor requires a Runnable object
+//		Names of threads can be set in and out of their constructor function
 		Thread t1 = new Thread(() -> {
 			for (int i = 0; i < 5; i++) {
-				System.out.println("Hi");
+				System.out.println("Hi has priority = "+Thread.currentThread().getPriority());
 				try { Thread.sleep(1000); } catch(Exception e) { e.printStackTrace(); }
 			}
-		});
+		}, "Hi Thread name set in Constructor");
 		Thread t2 = new Thread(() -> {
 			for (int i = 0; i < 5; i++) {
-				System.out.println("Hello");
+				System.out.println("Hello has priority = "+Thread.currentThread().getPriority());
 				try { Thread.sleep(1000); } catch(Exception e) { e.printStackTrace(); }
 			}
-		});
+		}, "Hello Thread name set in Constructor");
+//		t1.setName("Hi Thread name set outside Constructor");
+//		t2.setName("Hello Thread name set outside Constructor");
+
+		t1.setPriority(Thread.MIN_PRIORITY);
+		t2.setPriority(Thread.MAX_PRIORITY);
+
+		System.out.println(t1.getName());
+		System.out.println(t2.getName());
 
 		t1.start();
 		try { Thread.sleep(10); } catch(Exception e) { e.printStackTrace(); }
