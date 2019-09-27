a608df8454e079bb1e4d6d598896f14541500bb3
avirupdutta <finalguider99@gmail.com>
2019-09-27 19:22:19 +0530

    fully implemented queue DS

--- src/QueueDemo.java
+++ src/QueueDemo.java
@@ -37,31 +37,66 @@ class IntegerQueue{
         lastEntry--;
         return poppedElem;
     }
+    public boolean isEmpty() {
+        for (int elem: arr) {
+            if (elem != -1)
+                return false;
+        }
+        return true;
+    }
+    public int peek(){
+        return arr[0] != -1 ? arr[0]: null;
+    }
 }
 
 public class QueueDemo {
     public static void main ( String[] args ) {
         IntegerQueue intQueue = new IntegerQueue(5);
+
+//        prints false
         System.out.println("Is queue full ? " + intQueue.isFull());
 
+//        prints true
+        System.out.println(intQueue.isEmpty());
+
+//        iteratively filling up the queue
         for (int i = 10; i <= 50; i+=10){
             intQueue.insert(i);
         }
+
+//       Popping out all the data that were just inserted
         System.out.println(intQueue.pop());
         System.out.println(intQueue.pop());
         System.out.println(intQueue.pop());
         System.out.println(intQueue.pop());
         System.out.println(intQueue.pop());
+
+//        Inserting new data in the queue
         intQueue.insert(60);
         intQueue.insert(70);
         intQueue.insert(80);
         intQueue.insert(90);
         intQueue.insert(100);
-//        System.out.println(intQueue.pop());
-        System.out.println(intQueue.pop());
-        
 
+//        emptying the queue
+        System.out.println(intQueue.pop());
+        System.out.println(intQueue.pop());
+        System.out.println(intQueue.pop());
+        System.out.println(intQueue.pop());
+        System.out.println(intQueue.pop());
+
+
+//        produces -> java.lang.NullPointerException
+        try {
+            System.out.println("Peeked data: " + intQueue.peek());
+        } catch (Exception err){
+            System.out.println(err);
+        }
+
+//        displays the array
         System.out.println(Arrays.toString(intQueue.arr));
+
+//        prints false
         System.out.println("Is queue full ? " + intQueue.isFull());
     }
 }
